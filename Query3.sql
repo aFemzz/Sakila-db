0f724a67246c34ae4e2bc9ddbc33088512b0a43d
aFemzz <andhika.mailss@gmail.com>
2024-04-15 11:22:26 +0700

    chore : add docs to Query3 and clean up the code

--- Query3.sql
+++ Query3.sql
@@ -1,20 +1,17 @@
+-- GET TITLE, RENTAL RATE AND RATING
 SELECT title, rental_rate, rating
 FROM film
 WHERE rating IN ('G', 'R')
 AND rental_rate BETWEEN 2 AND 5
 ORDER BY title ASC;
 
-SELECT AVG(rental_duration) AS average_value_rental
-FROM film;
-
-SELECT rating FROM film
-AVG(rental_duration)
-
+-- GET AVG RATING AND AVG RENTAL DURATION
 SELECT rating
 , AVG (rental_duration)
 FROM film 
 GROUP BY rating
 
+-- GET DATA OF ACTOR AND FILM ID
 SELECT film.title , film_actor.actor_id, film.film_id
 FROM film
 JOIN film_actor ON film.film_id = film_actor.film_id
